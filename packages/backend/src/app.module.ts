5e0b68d4c2fb3bb1e0dd8067757f75d0e7b1a161
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-07-21 04:01:01 +0900

    feat: added user module

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [ConfigModule, TypeOrmModule],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [ConfigModule, TypeOrmModule.forRoot(), UserModule]
 })
 export class AppModule {}
