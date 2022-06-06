d7acb22e82897bba81341dfb82ff10104d69a4f5
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2022-06-07 00:13:32 +0900

    refactor: archi

--- internal/pkg/error/httperror/convert.go
+++ internal/pkg/error/httperror/convert.go
@@ -1,18 +1,18 @@
 package httperror
 
 import (
-	"errors"
 	"net/http"
 
+	"github.com/pkg/errors"
 	"go.uber.org/zap/zapcore"
 )
 
 // CodeToLevel ... convert grpc code to zapcore level
 func CodeToLevel(code int) zapcore.Level {
 	switch code {
-	case http.StatusNotFound, http.StatusBadRequest, http.StatusUnauthorized, http.StatusForbidden:
+	case http.StatusNotFound, http.StatusBadRequest, http.StatusUnauthorized, http.StatusForbidden, http.StatusServiceUnavailable:
 		return zapcore.WarnLevel
-	case http.StatusInternalServerError, http.StatusTooManyRequests, http.StatusServiceUnavailable:
+	case http.StatusInternalServerError, http.StatusTooManyRequests:
 		return zapcore.ErrorLevel
 	default:
 		return zapcore.InfoLevel
@@ -21,9 +21,19 @@ func CodeToLevel(code int) zapcore.Level {
 
 // ErrToCode ... convert err to code
 func ErrToCode(err error) int {
-	var notFoundError *NotFoundError
-	if ok := errors.As(err, &notFoundError); ok {
+	switch errors.Cause(err).(type) {
+	case ServiceUnavailableError:
+		return http.StatusServiceUnavailable
+	case UnauthorizedError:
+		return http.StatusUnauthorized
+	case ForbiddenError:
+		return http.StatusForbidden
+	case BadRequestError:
+		return http.StatusBadRequest
+	case NotFoundError:
 		return http.StatusNotFound
+	default:
 	}
+
 	return http.StatusInternalServerError
 }
