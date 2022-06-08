b9aaf78d1fa38bb6d088b9c53cdd047dfc485fb0
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2022-06-08 16:52:55 +0900

    fix: rename

---
+++ internal/pkg/glueerr/error.go
@@ -0,0 +1,8 @@
+package glueerr
+
+var (
+	InternalErr     InternalError     = "InternalErr"
+	NotFoundErr     NotFoundError     = "NotFoundErr"
+	UnauthorizedErr UnauthorizedError = "UnauthorizedErr"
+	BadRequestErr   BadRequestError   = "BadRequestErr"
+)
