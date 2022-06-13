8002b3388596904b83346c99d913f7caf0ca42e0
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2022-06-13 22:56:46 +0900

    chore: update lib

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,29 +1,22 @@
 import React from 'react'
-import { ExNextPageContext } from 'next'
 import Router from 'next/router'
-import { authorize } from '@boilerplate/fixtures'
-import { useAuthCookie, useSignIn, useSignOut } from '@boilerplate/fixtures'
+import { useSignIn, useSignOut, useIsLoggedIn } from 'src/modules/auth/hooks'
 
 type InitialProps = {}
 type Props = {} & InitialProps
 
 const Index = (_: Props) => {
-  const { idToken } = useAuthCookie()
   const { handleSignInWithGoogle } = useSignIn()
   const { handleSignOut } = useSignOut()
+  const { isLoggedIn } = useIsLoggedIn()
 
   return (
     <div>
       <button onClick={() => Router.push('/about')}>please click here!</button>
-      {!idToken && <button onClick={() => handleSignInWithGoogle()}>sign in</button>}
-      {idToken && <button onClick={() => handleSignOut()}>sign out</button>}
+      {!isLoggedIn && <button onClick={() => handleSignInWithGoogle()}>sign in</button>}
+      {isLoggedIn && <button onClick={() => handleSignOut()}>sign out</button>}
     </div>
   )
-}
-
-export const getServerSideProps = async (ctx: ExNextPageContext) => {
-  await authorize(ctx)
-  return { props: {} }
 }
 
 export default Index
